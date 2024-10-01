e56fc9928352753a4cc44bd9dc1426773282866e
Abdulsarhan <147336160+Abdulsarhan@users.noreply.github.com>
2024-10-01 13:24:45 -0400

    Added Contracts Cheats

---
+++ Cheats/CheatsHitmanContracts.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace CheatsForms
+{
+    partial class CheatsHitmanContracts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CheatsHitmanContracts));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.RTB_MainCheats = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.TB_Value = new System.Windows.Forms.TextBox();
+            this.B_SigScan = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.B_WriteToMemory = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.RTB_MainCheats, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(561, 206);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(4, 118);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(553, 41);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Level Select Menu\nEdit the hitman2.ini and change \"DefaultScene=AllLevels/logos.g" +
+    "ms\" to \"DefaultScene=AllLevels/levelmenu.gms”";
+            // 
+            // RTB_MainCheats
+            // 
+            this.RTB_MainCheats.Dock = System.Windows.Forms.DockStyle.Top;
+            this.RTB_MainCheats.Location = new System.Drawing.Point(4, 21);
+            this.RTB_MainCheats.Name = "RTB_MainCheats";
+            this.RTB_MainCheats.Size = new System.Drawing.Size(553, 90);
+            this.RTB_MainCheats.TabIndex = 2;
+            this.RTB_MainCheats.Text = resources.GetString("RTB_MainCheats.Text");
+            this.RTB_MainCheats.TextChanged += new System.EventHandler(this.RTB_MainCheats_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "The following cheats were tested with Steam release of the game:";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.20072F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.70886F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.TB_Value, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.B_SigScan, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.B_WriteToMemory, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 166);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(553, 33);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // TB_Value
+            // 
+            this.TB_Value.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TB_Value.Location = new System.Drawing.Point(105, 6);
+            this.TB_Value.Name = "TB_Value";
+            this.TB_Value.Size = new System.Drawing.Size(139, 20);
+            this.TB_Value.TabIndex = 1;
+            this.TB_Value.TextChanged += new System.EventHandler(this.TB_Value_TextChanged);
+            // 
+            // B_SigScan
+            // 
+            this.B_SigScan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.B_SigScan.Location = new System.Drawing.Point(307, 5);
+            this.B_SigScan.Name = "B_SigScan";
+            this.B_SigScan.Size = new System.Drawing.Size(75, 23);
+            this.B_SigScan.TabIndex = 2;
+            this.B_SigScan.Text = "Get a value";
+            this.B_SigScan.UseVisualStyleBackColor = true;
+            this.B_SigScan.Click += new System.EventHandler(this.B_SigScan_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Save files:";
+            // 
+            // B_WriteToMemory
+            // 
+            this.B_WriteToMemory.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.B_WriteToMemory.Location = new System.Drawing.Point(435, 5);
+            this.B_WriteToMemory.Name = "B_WriteToMemory";
+            this.B_WriteToMemory.Size = new System.Drawing.Size(97, 23);
+            this.B_WriteToMemory.TabIndex = 4;
+            this.B_WriteToMemory.Text = "Write To Memory";
+            this.B_WriteToMemory.UseVisualStyleBackColor = true;
+            this.B_WriteToMemory.Click += new System.EventHandler(this.B_WriteToMemory_Click);
+            // 
+            // CheatsHitmanContracts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 210);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CheatsHitmanContracts";
+            this.ShowIcon = false;
+            this.Text = "Hitman Contracts - Cheats";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox RTB_MainCheats;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TB_Value;
+        private System.Windows.Forms.Button B_SigScan;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button B_WriteToMemory;
+    }
+
+}
